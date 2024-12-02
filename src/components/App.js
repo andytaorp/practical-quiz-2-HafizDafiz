99e63a1e65b95d13196b2c672a91da59d1bd140c
Hafiz <23043274@myrp.edu.sg>
2024-12-02 14:25:43 +0800

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -7,19 +7,32 @@ function App() {
 
   const handleAddHabit = (habitName) => {
     // TODO: write code to add a new habit here
+    setHabits((prevHabits) => [...prevHabits, habitName]);
   };
 
   const handleToggleHabit = (id) => {
     // TODO: write code to toggle a habit's status
+    setHabits((prevHabits) => prevHabits.map((habitName) => 
+        habitName.id === id ? { ...habitName, completed: !habitName.completed } : habitName
+      )
+    );
+    
   };
 
   const handleDeleteHabit = (id) => {
     // TODO: write code to delete a habit
+    setHabits((prevHabits) => prevHabits.filter((habit) => habit.id !== id));
   };
 
   return (
     <div>
       <h1>Habit Tracker</h1>
+      <AddHabitForm onAddHabit={handleAddHabit} />
+      <HabitList
+      habits={habits}
+      onDeleteHabit={handleDeleteHabit}
+      onToggleHabit={handleToggleHabit}
+      />
       {/*TODO: add a form to add a new habit*/}
       {/*TODO: add a list of habits*/}
     </div>
